ed630be5f6b9a4e1ba17f15791e78e490c2f734f
Guillermo <gui@mail.com>
2026-01-16 22:33:12 +0100

    Adding port

--- collector.py
+++ collector.py
@@ -7,6 +7,7 @@ DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 DB_NAME = os.getenv("DB_NAME")
 DB_PORT = os.getenv("DB_PORT")
+DB_PORT = env_int("DB_PORT", 27533)
 
 AS_OF = dt.date.today()
 
